91aa1c2783cdb88b9d3ea2a2754fcd30a8b00f0d
luofeng457 <luofeng457@163.com>
2019-11-08 14:20:40 +0800

    add demo -- redux-saga

---
+++ demo06-redux-saga/src/stores/index.js
@@ -0,0 +1,18 @@
+import reducer from '../reducers/index';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import mySaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__({}) : compose;
+const enhancer = compose(applyMiddleware(sagaMiddleware));
+
+const store = createStore(
+    reducer,
+    enhancer,
+);
+
+sagaMiddleware.run(mySaga);
+
+export default store;
