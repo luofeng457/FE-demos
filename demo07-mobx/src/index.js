e34a2e478b20182147ea373c102cdce3240fc691
luofeng457 <luofeng457@163.com>
2019-11-09 16:05:16 +0800

    bugfixed on demo07-mobx

--- demo07-mobx/src/index.js
+++ demo07-mobx/src/index.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { observable, action } from 'mobx'
 import { observer } from 'mobx-react';
 
+
 // observeable 事件生产者，设定需要变更的状态，通过action派发事件更新状态
-let counterState = observable({
+let appState = observable({
     count: 0,
 })
 
-counterState.handleClick = action(() => {
-    counterState.count += 1;
+appState.handleClick = action(() => {
+    appState.count += 1;
 })
 
 // @observer 事件订阅者，把需要更新的组件作为观察者，会在渲染的时候自动追踪observable的变化
 const Counter = observer(() => {
-    const [ count, setCount ] = useState(0);
-    return <button onClick={() => setCount(count + 1)}>{count}</button>
+    return <button onClick={appState.handleClick}>{appState.count}</button>
 })
 
-ReactDOM.render(<Counter counterState={counterState} />, document.getElementById('root'))
+ReactDOM.render(<Counter appState={appState} />, document.getElementById('root'))
