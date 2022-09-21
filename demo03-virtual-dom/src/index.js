13a4aa3ad2dc27911310ce68fcd8f94bb247e628
luofeng457 <luofeng457@163.com>
2022-09-21 22:41:58 +0800

    feat: virtual dom

--- demo03-virtual-dom/src/index.js
+++ demo03-virtual-dom/src/index.js
@@ -10,7 +10,7 @@ let v_dom = createElement('ul', {className: 'list', style: "color: green", value
 ])
 let v_dom_2 = createElement('ul', {style: "color: green", value: 123}, [
     createElement('li', {className: 'important'}, ['react']),
-    createElement('li', {}, ['Angular']),
+    createElement('li', {style: "color: red"}, ['Angular']),
 ])
 
 let el = render(v_dom);
